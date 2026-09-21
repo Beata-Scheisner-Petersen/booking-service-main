b6fccb1f08e5579aafd28ce40cfb47b9eaebc3b5
soppitopp <soppitopp2000@hotmail.com>
2026-09-21 13:21:43 +0200

    fix errors in tests workflow, add applications to tests

--- src/test/java/service/booking/customerapi/ConnectionControllerTest.java
+++ src/test/java/service/booking/customerapi/ConnectionControllerTest.java
@@ -4,7 +4,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.*;
@@ -15,8 +15,15 @@ import java.nio.charset.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest()
-@org.springframework.test.context.ActiveProfiles("connection")
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:mysql://localhost:3307/Booking-service",
+        "spring.datasource.username=booking_user",
+        "spring.datasource.password=booking_password",
+        "REVIEW_DB_CLIENT_URL=http://localhost:8081",
+        "JWT_SECRET=bu5HenKK9pCurkUUic604aWzpvY4XruaVZsIkArn0EE9M9GFfTX1vq3vSubM1gwyrZB0Fs22yv5XsWt8jxbT8h"
+})
+
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ConnectionControllerTest {
