e7e91161b4044ea0d7d3997d778a0127e22bc12e
soppitopp <soppitopp2000@hotmail.com>
2026-09-21 13:37:40 +0200

    removed properties again

--- src/test/java/service/booking/customerapi/ConnectionControllerTest.java
+++ src/test/java/service/booking/customerapi/ConnectionControllerTest.java
@@ -16,13 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:mysql://localhost:3307/Booking-service",
-        "spring.datasource.username=booking_user",
-        "spring.datasource.password=booking_password",
-        "REVIEW_DB_CLIENT_URL=http://localhost:8081",
-        "JWT_SECRET=bu5HenKK9pCurkUUic604aWzpvY4XruaVZsIkArn0EE9M9GFfTX1vq3vSubM1gwyrZB0Fs22yv5XsWt8jxbT8h"
-})
+@SpringBootTest()
 
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
